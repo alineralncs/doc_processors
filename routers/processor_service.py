957d3576a0a63db61a0e0b6ee7bdf6cc5aee85e2
Aline Rose <aline.rose@mail.uft.edu.br>
2023-09-24 14:04:23 -0300

    remocao de stopwords e outros, padronizacao data, add resolucao

--- routers/processor_service.py
+++ routers/processor_service.py
@@ -2,7 +2,6 @@ import re
 
 import PyPDF2
 import spacy
-from dateutil import parser
 from docarray import DocumentArray
 
 nlp = spacy.load("pt_core_news_sm")
@@ -27,39 +26,99 @@ class ProcessorService:
 
         return self.text
 
+    def preprocess_text(self) -> str:
+        # pre processar o texto do documento
+        # retirar caracteres especiais
+        # retirar pontuacoes
+        # retirar stopwords
+        from nltk.corpus import stopwords
+
+        stopwords = stopwords.words("portuguese")
+        stopwords_personalizadas = [
+            "universidade federal tocantins",
+            "fundação universidade federal tocantins",
+            "uft",
+            "universidad federal tocantins",
+        ]
+        text = ""
+        doc = nlp(self.text)
+        text_formatted = spacy.tokens.Doc(
+            doc.vocab,
+            words=[
+                token.text.lower()
+                for token in doc
+                if not token.is_space
+                and not token.is_punct
+                and not (
+                    token.text.lower() in stopwords and token.text.lower() != "conselho"
+                )
+                and token.text.lower() not in stopwords_personalizadas
+            ],
+        )
+        text_formatted = " ".join([token.text for token in text_formatted])
+        return text_formatted
+
     def classification(self, text) -> str:
         # classificar o documento dividir entre consiuni e consepe
 
         if "consuni" in text.lower() or "conselho universitário" in text.lower():
             return "Consuni"
-        if "consepe" in text.lower() or "conselho de ensino" in text.lower():
+        if "consepe" in text.lower() or "conselho ensino" in text.lower():
             return "Consepe"
         return "Não classificado"
 
-    def extract_publication_date(self) -> str:
+    def extract_publication_date(self, text) -> str:
         # extrair a data de publicação do documento
         if not self.text:
             self.extract_text()
         datas = []
-        date_pattern = r"\d{1,2} de [a-zA-Z]+ de \d{4}"
-        documents = self.text.split("\n")
+        date_pattern = r"palmas\s\d+\s[a-zA-Z]+\s\d{4}"
+        documents = text.split("\n")
         for document in documents:
             match = re.search(date_pattern, document)
             if match:
-                datas.append(match.group())
+                # verificar demais grupos
+                day = match.group().split(" ")[1]
+                # print(day)
+                month = match.group().split(" ")[2]
+                # print(month)
+                year = match.group().split(" ")[3]
+                # print(year)
+                month_dict = {
+                    "janeiro": "01",
+                    "fevereiro": "02",
+                    "março": "03",
+                    "abril": "04",
+                    "maio": "05",
+                    "junho": "06",
+                    "julho": "07",
+                    "agosto": "08",
+                    "setembro": "09",
+                    "outubro": "10",
+                    "novembro": "11",
+                    "dezembro": "12",
+                }
+                month = month_dict[month.lower()]
+                date = f"{day}/{month}/{year}"
+
+                datas.append(date)
 
         return datas
 
     def extract_signatures(self, text) -> str:
         # extrair as assinaturas do documento
-        prof_pattern = r"Prof\.\s[A-Z][a-z]+\s[A-Z][a-z]+"
-        reitor_pattern = r"[A-Z][A-ZÁ-ÚÉÊÓÔÍ][A-ZÁ-ÚÉÊÓÔÍa-zá-úéêóôí]+\sReitor"
+
+        prof_pattern = r"prof\s+([a-z]+\s+[a-z]+)\s+presidente"
+
+        reitor_pattern = r"[A-Z][A-ZÁ-ÚÉÊÓÔÍ][A-ZÁ-ÚÉÊÓÔÍa-zá-úéêóôí]+\sreitor"
 
         professor_match = re.search(prof_pattern, text)
         if professor_match:
-            return professor_match.group()
+            nome = professor_match.group().split(" ")[1]
+            sobrenome = professor_match.group().split(" ")[2]
 
-        # Verifique se o texto contém a assinatura de um reitor
+            return f"{nome} {sobrenome}"
+
         reitor_match = re.search(reitor_pattern, text)
         if reitor_match:
             return reitor_match.group()
@@ -73,8 +132,18 @@ class ProcessorService:
         # remover dados desnecessarios do documento
         pass
 
-    def extract_resolutions(self) -> str:
+    def extract_resolutions(self, text) -> str:
         # extrair as resoluções do documento
+        # pattern = r"N[º°]\s?\d+\s*/\s*\d+"
+        pattern = r"[Nn]\s?[º°]\s?\d+\s*/\s*\d+"
+        match = re.search(pattern, text, re.IGNORECASE)
+
+        if match:
+            resolution_number = match.group()
+            return resolution_number
+        else:
+            return None
+
         pass
 
     def combination(self):
@@ -82,9 +151,11 @@ class ProcessorService:
 
         if not self.text:
             self.extract_text()
-        dates = self.extract_publication_date()
-        classification = self.classification(self.text)
-        signature = self.extract_signatures(self.text)
+        texto_formatado = self.preprocess_text()
+        dates = self.extract_publication_date(texto_formatado)
+        classification = self.classification(texto_formatado)
+        signature = self.extract_signatures(texto_formatado)
+        resolution = self.extract_resolutions(texto_formatado)
 
         for i, document in enumerate(self.doc_array):
             pdf_path = document.tags.get("path")
@@ -92,9 +163,11 @@ class ProcessorService:
                 date = dates[i] if i < len(dates) else None
                 infos_return = {
                     "pdf_path": pdf_path,
+                    "texto": texto_formatado,
                     "dates": date,
                     "classification": classification,
                     "signature": signature,
+                    "resolution": resolution,
                 }
 
         return infos_return
