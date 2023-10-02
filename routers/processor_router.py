2e317ba9a88be7da46189fccd786d9e6d8dc7cd6
Victor Hugo C. Gomes <vhclaudiogomes@gmail.com>
2023-10-02 16:37:41 -0300

    adicionado documentos e algumas mudanças

--- routers/processor_router.py
+++ routers/processor_router.py
@@ -10,18 +10,17 @@ router = APIRouter()
 
 @router.get("/preprocess")
 def preprocess_documents():
-    root_directory = "documents_test"
+    root_directory = "documents_new"
     results = []
     # chamar funcao de pre processamento
-    for year in range(2004, 2015):
+    for year in range(2014, 2024):
         for folder_name in ["Consuni", "Consepe"]:
             folder_path = os.path.join(root_directory, folder_name, str(year))
-            # print(folder_path)
 
             for root, dirs, files in os.walk(folder_path):
                 for filename in files:
                     pdf_path = os.path.join(root, filename)
-                    # print(pdf_path)
+                    print(pdf_path)
 
                     doc_array = DocumentArray()
                     doc_array.append(Document(path=pdf_path))
@@ -29,5 +28,8 @@ def preprocess_documents():
                     processor_service = ProcessorService(doc_array)
 
                     results.append(processor_service.combination())
+    for x in results:
+        print(x)
+        print('\n\n\n')
 
     return {"results": results}
